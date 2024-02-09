da234100649cec29b22e0b80ed998c6b219c4d3b
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2024-02-09 09:11:18 -0300

    config_project
    Basic Maven configuration

---
+++ src/test/java/com/etmadev/certification_nlw/CertificationNlwApplicationTests.java
@@ -0,0 +1,14 @@
+package com.etmadev.certification_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
