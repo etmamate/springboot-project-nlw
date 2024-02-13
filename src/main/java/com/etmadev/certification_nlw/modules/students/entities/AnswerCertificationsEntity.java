dd37f27bdfb2ecaf38316e14cf735a3b5638177f
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2024-02-13 12:24:58 -0300

    Update AnswerCertificationsEntity.java

--- src/main/java/com/etmadev/certification_nlw/modules/students/entities/AnswerCertificationsEntity.java
+++ src/main/java/com/etmadev/certification_nlw/modules/students/entities/AnswerCertificationsEntity.java
@@ -2,6 +2,13 @@ package com.etmadev.certification_nlw.modules.students.entities;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +16,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswerCertificationsEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @JoinColumn(name = "certification_id")
     private UUID certificationID;
+    
+    @JoinColumn(name = "student_id")
     private UUID studentID;
+
+    @JoinColumn(name = "question_id")
     private UUID questionID;
+
+    @JoinColumn(name = "answer_id")
     private UUID answerId;
+
+    @JoinColumn(name = "is_correct")
     private boolean isCorrect;
+
+    
 
 }
