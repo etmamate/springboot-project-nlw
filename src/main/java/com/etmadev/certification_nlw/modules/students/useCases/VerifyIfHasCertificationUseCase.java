22ba75f422e9780980eecfd9ab0937303a7f014c
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2024-02-14 09:56:40 -0300

    .

--- src/main/java/com/etmadev/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/etmadev/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,17 +1,25 @@
 package com.etmadev.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.etmadev.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.etmadev.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
-    
 
-    public boolean execute( VerifyHasCertificationDTO dto ){
-        if(dto.getEmail().equals("mateuscristosilva132@gmail.com") && dto.getTechnology().equals("JAVA")){
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    public boolean execute(VerifyHasCertificationDTO dto) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+                dto.getTechnology());
+
+        if (!result.isEmpty()) {
             return true;
         }
+
         return false;
     }
 }
